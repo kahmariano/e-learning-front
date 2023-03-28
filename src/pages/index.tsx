99d8dc67f8ed1c93bb6a1aeb25d367ca0388adc6
Karina Mariano <kmariano138@gmail.com>
2023-03-27 23:25:25 -0300

    login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,16 @@
 export default function Home(): JSX.Element {
   return (
     <>
-      <div className="text-3xl font-bold underline "> Te amo Filipe Naidai</div>
+      <div className="bg-purple-700 h-screen	w-full">
+        {/* <div> */}
+        <h1 className="text-pink-500 text-2xl">Aprenda da melhor forma</h1>
+        <h2 className="text-white">
+          Entre na plataforma e acesse cursos de diversas áreas de conhecimento.
+        </h2>
+        <button className="bg-pink-500 text-white font-medium p-4 rounded-full w-64">
+          Começar os estudos
+        </button>
+      </div>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,29 @@
+const colors = require('tailwindcss/colors')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        ...colors,
+        purple: {
+          ...colors.purple,
+          700: "#6548A3",
+        },
+        pink: {
+          ...colors.pink,
+          500: "#FF6680",
+        }
+      },
+    },
   },
   plugins: [],
 }
